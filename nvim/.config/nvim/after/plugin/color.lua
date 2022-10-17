95545ccfad66749b54e7c580c8b47e9d4eaab59c
aguiarjv <jvictoraguiar@live.com>
2022-10-17 11:39:48 -0300

    updating

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -3,13 +3,16 @@ vim.g.tokyonight_transparent = true
 
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.g.gruvbox_invert_selection = '0'
+vim.g.gruvbox_sign_column = 'none'
 
 vim.opt.background = "dark"
 
 vim.cmd("colorscheme tokyonight-moon")
+-- vim.cmd("colorscheme gruvbox")
 
 vim.cmd(":hi normal guibg=000000") -- Transparent background
 vim.cmd(":highlight SignColumn guibg=NONE") -- Transparent SignColumn
+vim.cmd(":highlight CursorLineNR guibg=NONE") -- Transparent Number Column
 
 
 -- Status line config
---
+++ nvim/.config/nvim/test.py
@@ -0,0 +1,12 @@
+import math
+
+
+def test_func():
+    c = 2
+    b = 4
+
+    return c + b
+
+
+if __name__ == "__main__":
+    test_func()
