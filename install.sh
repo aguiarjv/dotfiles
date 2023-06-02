972a0e414de1a71fd2e84cf518f596c1eaf9b696
aguiarjv <jvictoraguiar@live.com>
2023-06-01 22:14:47 -0300

    fixing install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ mkdir -p ~/work/
 
 mkdir -p ~/.config/i3/
 mkdir -p ~/.config/i3status/
-mkdir -p ~/.config/nvim/
 
 mkdir -p ~/Pictures/OP-Wallpapers/
 
@@ -29,7 +28,7 @@ sudo mkdir -p /media/$USER_NAME/usb/
 #sudo apt install htop ntfs-3g xclip -y
 #sudo apt install ranger feh build-essentials -y
 
-sudo apt install i3 i3status dmenu thunar xfce4-terminal tmux fzf ripgrep nodejs htop ntfs-3g xclip ranger feh build-essential -y
+#sudo apt install i3 i3status dmenu thunar xfce4-terminal tmux fzf ripgrep nodejs htop ntfs-3g xclip ranger feh build-essential -y
 
 
 # Moving files
