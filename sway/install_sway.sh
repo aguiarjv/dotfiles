31ea4d8645c540b2d22afd5a3c2e3506d34828fc
Joao Aguiar <joao.aguiar@intelie.com>
2025-08-13 11:37:07 -0300

    added blueman to sway installation

--- sway/install_sway.sh
+++ sway/install_sway.sh
@@ -14,7 +14,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 #sudo apt install pavucontrol -y
 #sudo apt install grim slurp wl-clipboard -y
 #sudo apt install pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr -y
-#sudo apt install sway swaybg swayidle swayimg swaylock sway-notification-center light waybar wofi fonts-font-awesome wdisplays thunar pavucontrol grim slurp wl-clipboard pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr -y
+#sudo apt install blueman -y
+#sudo apt install sway swaybg swayidle swayimg swaylock sway-notification-center light waybar wofi fonts-font-awesome wdisplays thunar pavucontrol grim slurp wl-clipboard pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr blueman -y
 
 # Moving files
 cp $SCRIPT_DIR/swayconfig ~/.config/sway/config
